5b670a3915e742c1603a10a3a7a1749edc57b468
Eiko.dev <samuel.lol2019a@hotmail.com>
2023-05-10 19:37:41 -0300

    Eikodev

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import { Ubuntu } from 'next/font/google'
 const ubuntu = Ubuntu({weight: ['400','700','500','300'], style: 'normal', subsets: ['latin']})
 
 export const metadata = {
-  title: 'Samuel Silva (a.k.a Eiko)',
+  title: 'Eiko',
   description: 'Software Engineer and Backend Developer.',
 }
 
