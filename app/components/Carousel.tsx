a1639d448ced596d1627052fc74575bc2227f36d
Eiko.dev <samuel.lol2019a@hotmail.com>
2023-05-12 21:58:04 -0300

    Eikodev

---
+++ app/components/Carousel.tsx
@@ -0,0 +1,112 @@
+"use client"
+import "./styles.css"
+import { useKeenSlider } from "keen-slider/react"
+import "keen-slider/keen-slider.min.css"
+import { useState } from "react"
+
+export function Carousel() {
+    const [currentSlide, setCurrentSlide] = useState(0)
+    const [loaded, setLoaded] = useState(false)
+    const [sliderRef, instanceRef] = useKeenSlider<HTMLDivElement>({
+        loop: false,
+        initial: 0,
+        slideChanged(slider) {
+            setCurrentSlide(slider.track.details.rel)
+        },
+        created() {
+            setLoaded(true)
+        },
+    })
+
+    return (
+        <>
+            <div className="navigation-wrapper">
+                <div ref={sliderRef} className="keen-slider">
+                    <div className="keen-slider__slide number-slide1">
+                        <section className='flex flex-col text-center prose-invert prose-p:px-5 prose-p:mb-4 pb-28 md:prose-p:px-0'>
+                            <h1 className='text-3xl font-bold mb-4 text-center'>Featured open-source projects</h1>
+                            <div className='flex justify-center pt-5 not-prose'>
+                                <div className='w-[22rem] md:w-[70rem] md:h-[520px]'>
+                                    <div className='flex flex-row justify-center'>
+                                        <div className='color flex flex-row rounded-3xl h-[520px] text-left min-w-[1120px max-w-[1120px] gradiant-color relative overflow-hidden' style={{}} >
+                                            <div className='p-8'>
+                                                <h1 className='font-syne font-bold text-4xl md:text-5xl pb-5'>Lobinho</h1>
+                                                <p className='text-base mb-4 md:text-lg'>A Discord Bot based on the League of Legends (LOL) game, use the bot to manage and create tickets.!</p>
+                                                <p className='text-base mb-4 md:text-lg'>Built in 2020 as a toy project with some friends, shut down and archived in 2021. Built for a server with 40K+ Members. I made a lot of architectural mistakes and also learned a lot while doing it.</p>
+                                                <h3 className='font-bold text-xl pt-5 mb-2'>Technologies</h3>
+                                                <div className='flex flex-row'>
+                                                    <svg width="56" height="56" className="w-6 h-6 mr-2 rounded-sm" style={{ fill: "white" }} viewBox="0 0 256 282" xmlns="http://www.w3.org/2000/svg"><title>Node.js</title><g><path d="M116.504 3.58c6.962-3.985 16.03-4.003 22.986 0 34.995 19.774 70.001 39.517 104.99 59.303 6.581 3.707 10.983 11.031 10.916 18.614v118.968c.049 7.897-4.788 15.396-11.731 19.019-34.88 19.665-69.742 39.354-104.616 59.019-7.106 4.063-16.356 3.75-23.24-.646-10.457-6.062-20.932-12.094-31.39-18.15-2.137-1.274-4.546-2.288-6.055-4.36 1.334-1.798 3.719-2.022 5.657-2.807 4.365-1.388 8.374-3.616 12.384-5.778 1.014-.694 2.252-.428 3.224.193 8.942 5.127 17.805 10.403 26.777 15.481 1.914 1.105 3.852-.362 5.488-1.274 34.228-19.345 68.498-38.617 102.72-57.968 1.268-.61 1.969-1.956 1.866-3.345.024-39.245.006-78.497.012-117.742.145-1.576-.767-3.025-2.192-3.67-34.759-19.575-69.5-39.18-104.253-58.76a3.621 3.621 0 0 0-4.094-.006C91.2 39.257 56.465 58.88 21.712 78.454c-1.42.646-2.373 2.071-2.204 3.653.006 39.245 0 78.497 0 117.748a3.329 3.329 0 0 0 1.89 3.303c9.274 5.259 18.56 10.481 27.84 15.722 5.228 2.814 11.647 4.486 17.407 2.33 5.083-1.823 8.646-7.01 8.549-12.407.048-39.016-.024-78.038.036-117.048-.127-1.732 1.516-3.163 3.2-3 4.456-.03 8.918-.06 13.374.012 1.86-.042 3.14 1.823 2.91 3.568-.018 39.263.048 78.527-.03 117.79.012 10.464-4.287 21.85-13.966 26.97-11.924 6.177-26.662 4.867-38.442-1.056-10.198-5.09-19.93-11.097-29.947-16.55C5.368 215.886.555 208.357.604 200.466V81.497c-.073-7.74 4.504-15.197 11.29-18.85C46.768 42.966 81.636 23.27 116.504 3.58z"></path><path d="M146.928 85.99c15.21-.979 31.493-.58 45.18 6.913 10.597 5.742 16.472 17.793 16.659 29.566-.296 1.588-1.956 2.464-3.472 2.355-4.413-.006-8.827.06-13.24-.03-1.872.072-2.96-1.654-3.195-3.309-1.268-5.633-4.34-11.212-9.642-13.929-8.139-4.075-17.576-3.87-26.451-3.785-6.479.344-13.446.905-18.935 4.715-4.214 2.886-5.494 8.712-3.99 13.404 1.418 3.369 5.307 4.456 8.489 5.458 18.33 4.794 37.754 4.317 55.734 10.626 7.444 2.572 14.726 7.572 17.274 15.366 3.333 10.446 1.872 22.932-5.56 31.318-6.027 6.901-14.805 10.657-23.56 12.697-11.647 2.597-23.734 2.663-35.562 1.51-11.122-1.268-22.696-4.19-31.282-11.768-7.342-6.375-10.928-16.308-10.572-25.895.085-1.619 1.697-2.748 3.248-2.615 4.444-.036 8.888-.048 13.332.006 1.775-.127 3.091 1.407 3.182 3.08.82 5.367 2.837 11 7.517 14.182 9.032 5.827 20.365 5.428 30.707 5.591 8.568-.38 18.186-.495 25.178-6.158 3.689-3.23 4.782-8.634 3.785-13.283-1.08-3.925-5.186-5.754-8.712-6.95-18.095-5.724-37.736-3.647-55.656-10.12-7.275-2.571-14.31-7.432-17.105-14.906-3.9-10.578-2.113-23.662 6.098-31.765 8.006-8.06 19.563-11.164 30.551-12.275z"></path></g></svg>
+                                                    <svg width="120" height="258" viewBox="0 0 120 258" className="w-6 h-6 mr-2 rounded-sm" fill="none" xmlns="http://www.w3.org/2000/svg"><title>MongoDB</title><path d="M83.0089 28.7559C72.1328 15.9086 62.7673 2.86053 60.8539 0.150554C60.6525 -0.0501848 60.3503 -0.0501848 60.1489 0.150554C58.2355 2.86053 48.8699 15.9086 37.9938 28.7559C-55.3594 147.292 52.6968 227.287 52.6968 227.287L53.6031 227.889C54.4087 240.235 56.4228 258 56.4228 258H60.451H64.4792C64.4792 258 66.4934 240.335 67.299 227.889L68.2052 227.187C68.306 227.187 176.362 147.292 83.0089 28.7559ZM60.451 225.48C60.451 225.48 55.6172 221.365 54.3081 219.257V219.057L60.1489 89.9813C60.1489 89.5798 60.7532 89.5798 60.7532 89.9813L66.594 219.057V219.257C65.2848 221.365 60.451 225.48 60.451 225.48Z" fill="white"></path></svg>
+                                                </div>
+                                                <div className='flex flex-col font-bold pt-8'>
+                                                    <a href="" className='mr-10'>
+                                                        Github
+                                                        <svg width="24" height="24" className="inline h-4 w-4" style={{ stroke: "white" }} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 13V19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V8C3 7.46957 3.21071 6.96086 3.58579 6.58579C3.96086 6.21071 4.46957 6 5 6H11" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M15 3H21V9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M10 14L21 3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                                                    </a>
+                                                    <a href="https://lobinhobot.vercel.app" className='mr-10'>
+                                                        WebSite
+                                                        <svg width="24" height="24" className="inline h-4 w-4" style={{ stroke: "white" }} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M18 13V19C18 19.5304 17.7893 20.0391 17.4142 20.4142C17.0391 20.7893 16.5304 21 16 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V8C3 7.46957 3.21071 6.96086 3.58579 6.58579C3.96086 6.21071 4.46957 6 5 6H11" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M15 3H21V9" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M10 14L21 3" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></svg>
+                                                    </a>
+                                                </div>
+                                            </div>
+
+                                            <div className='absolute right-0 -z-10' >
+                                                <img src="/background.jpg" alt="" className='hidden object-cover w-full md:inline md:h-full md:rounded-r-3xl opacity-10' />
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                </div>
+                {loaded && instanceRef.current && (
+                    <>
+                        <Arrow
+                            left
+                            onClick={(e: any) =>
+                                e.stopPropagation() || instanceRef.current?.prev()
+                            }
+                            disabled={currentSlide === 0}
+                        />
+
+                        <Arrow
+                            onClick={(e: any) =>
+                                e.stopPropagation() || instanceRef.current?.next()
+                            }
+                            disabled={
+                                currentSlide ===
+                                instanceRef.current.track.details.slides.length - 1
+                            }
+                        />
+                    </>
+                )}
+            </div>
+        </>
+    )
+}
+
+function Arrow(props: {
+    disabled: boolean
+    left?: boolean
+    onClick: (e: any) => void
+}) {
+    const disabeld = props.disabled ? " arrow--disabled" : ""
+    return (
+        <svg
+            onClick={props.onClick}
+            className={`arrow ${props.left ? "arrow--left" : "arrow--right"
+                } ${disabeld}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+        >
+            {props.left && (
+                <path d="M16.67 0l2.83 2.829-9.339 9.175 9.339 9.167-2.83 2.829-12.17-11.996z" />
+            )}
+            {!props.left && (
+                <path d="M5 3l3.057-3 11.943 12-11.943 12-3.057-3 9-9z" />
+            )}
+        </svg>
+    )
+}
